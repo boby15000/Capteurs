4c17f5485c0d232283ac9d4da23897c8b884c526
Nicolas Fourgheon <boby15000@hotmail.com>
2025-01-09 22:50:20 +0400

    Ajout de  l'exemple

--- include/Capteurs.h
+++ include/Capteurs.h
@@ -8,15 +8,21 @@
 #define Capteurs_INCLUDED
 
 
-class Capteurs
-{
-  public :
-    
-    
+class Capteurs 
+{ 
+    public: 
+        Capteurs(int Transmetteur);
+        float Value(); 
+        void SetParametre(float tensionMin, float tensionMax, float plageCapteurMin, float plageCapteurMax);
+        float Pourcentage();
+        float PourcentageInverse();
 
-  private :
-
-
+    private: 
+        int transmetteur;
+        float TensionMin;
+        float TensionMax;
+        float PlageCapteurMin;
+        float PlageCapteurMax;
 };
 
 #endif
--- src/Capteurs.cpp
+++ src/Capteurs.cpp
@@ -3,3 +3,55 @@
 #endif
 
 
+int Transmetteur; // Pin du capteur
+float TensionMin = 0 ; // Tension minimum de sortie proportionnel au Capteur (équivalent au 0bar)
+float TensionMax = 5.0 ; // Tension maximum de sortie proportionnel au Capteur (équivalent au 10bar)
+float PlageCapteurMin = 0 ; // Valeur basse du transmetteur (par défaut capteur 0-10bar)
+float PlageCapteurMax = 10 ; // Valeur haute du transmetteur (par défaut capteur 0-10bar)
+
+Capteurs::Capteurs(int transmetteur)
+  {
+      Transmetteur = transmetteur ;
+      pinMode(Transmetteur, INPUT);
+  }
+
+  // Modifie les paramètres de calcul de la Capteurs
+  void Capteurs::SetParametre(float tensionMin, float tensionMax, float plageCapteurMin, float plageCapteurMax)
+  {
+    TensionMin = tensionMin ;
+    TensionMax = tensionMax ; 
+    PlageCapteurMin = plageCapteurMin ; 
+    PlageCapteurMax = plageCapteurMax ;
+  }
+
+
+  /**
+   * Fonction de calcul de Capteurs
+   * Formules : ((Tension mesuré - Tension Minimum) * Ecart de P°) / Ecart de Tension)
+   * Parametres :
+   * TensionMin : Tension minimum
+   * TensionMax : Tension maximum
+   * PlageCapteurMin : Plage Basse du capteur
+   * PlageCapteurMax : Plage haute du capteur
+   * Value : Mesure de l'entrée annalogique
+   */
+  float Capteurs::Value()
+  {
+    // Convertie la valeur analogique en Volt.
+    float resultVolts = (analogRead(Transmetteur) / 1023.0 )* 5.0 ;
+    
+    // Returne la valeur du Capteurs.
+    return ((resultVolts-TensionMin) * (PlageCapteurMax - PlageCapteurMin ) / (TensionMax - TensionMin)) ;
+  }
+
+
+  float Capteurs::Pourcentage()
+  {
+    return ( (Value() * 100) / PlageCapteurMax);
+  }
+
+
+  float Capteurs::PourcentageInverse()
+  {
+    return ( 100 - Pourcentage() );
+  }
